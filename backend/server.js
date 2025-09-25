50be27c20b2eea7705f050e6601358304987599f
Rayssa <rayssaaquino77@gmail.com>
2025-09-25 09:41:10 -0300

    criando a estrutura de codigo do server

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,74 @@ const path = require("path") //caminho do arquinho
 const bcrypt = require("bcript");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-
 const app = express();
 const port = 5001;
-
 app.use(cors());
 app.use(express.json());
 
+//criar uma string para renovar a chave da autenticação
+
+const SECRET_KEY = "12345678910";
+
+//local onde esta o arquivo do banco de dados
+
+const localDados = path.join(__dirname,'data/usuarios.json');
+
+// função para ler os dados do arquivo 
+
+const consultarUsuarios=()=>{
+    const data = fs.readFileSync(localDados, "utf-8");
+    return JSON.parse(data);
+}
+
+//função para gravar dados no arquivo
+
+const salvarUsuarios=()=>{
+    fs.writeFileSync(localDados,JSON.stringify(users,null,2))
+}
+
+//executando o servidor na porta definida
 app.listen(port,()=>{
     console.log(`Servidor rodando https://localhost:${port}`)
 })
 
+//rota para regristar o usuario
+
+app.post("/register", async(req,res)=>{
+    const {email,senha} = req.body;
+
+    if(!email || !senha){
+        return res.status(400).json({message:"Campos obrigatorios"})
+    }
+    const users = consultarUsuarios();
+    if(users.find(user=>user.email == email)){
+        return res.status(400).json({message: "Email já cadastrado no banco de dados"})
+    }
+
+    //criptografar a senha
+    const hashSenha = await bcrypt.hash(senha,10)
+    const novoUsuario = {id:Date.now, email,senha:hashSenha};
+    users.push(novoUsuario);
+    salvarUsuarios(users);
+    res.status(200).json({message: "Usuario registrado com sucesso"})
+})
+
+// ROTA DO LOGIN
+
+app.post("/login", async (req,res)=>{
+    const {email, senha}= req.body;
+    const users = consultarUsuarios();
+    const user = users.find(user=>user.email === email);
+
+    if(!user){
+        return res.status(400).json({message: "Usuário/senha Inválidos"})
+    }
+    const senhaValida= await bcrypt.compare(senha, user.senha);
+    if(!senhaValida){
+         return res.status(400).json({message: "Senha inválida"})
+    }
+    // AUTENTICAÇÃO DO JWT
+    const token = jwt.sign({id:user.id,email:user.email},SECRET_KEY,{expiresIn:"10m"});
+    res.json({message:"Login realizado com sucesso",token})
+
+})
